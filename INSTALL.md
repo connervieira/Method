41be23f3ce217703eb1f0440d76829d805921c77
Conner Vieira <connervieira@tutanota.com>
2021-11-15 21:12:10 -0500

    Added extremely simple installation instructions

---
+++ INSTALL.md
@@ -0,0 +1,8 @@
+# Install
+
+Installing Method is a simple, step by step process.
+
+1. Download Method
+2. Install Python3
+3. Install BitcoinPython Python library with pip
+4. Install PyQRCode Python library with pip
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from bitcoinpython import Key
 from os import system, name
+from bitcoinpython.network import satoshi_to_currency
+import pyqrcode
 
 
 # ----- Configuration Start -----
 
 private_key = "" # The BitcoinCash wallet key in hex form.
-generate_key = False # Generate a Bitcoin Cash private key, then exit.
 
 # ----- Configuration End -----
 
@@ -24,39 +25,53 @@ def clear():
 clear() # Clear the screen on first launch
 
 
-if (generate_key == True):
+# Load the private key from the configuration
+if (private_key == ""):
+    # If no private key has been set, generate one automatically.
     k = Key()
-    print("Generated private key: " + k.to_hex())
+    print("Warning: No private key has been set. A temporary one has been generated, but this will change on next launch. Before funding the ATM, configure a static private key.")
+    input("Press enter to continue")
+    clear()
 else:
-    # Load the private key from the configuration
+    # If a private key has been set, load it.
     k = Key.from_hex(private_key)
 
 
-    # After intial start-up, ask what mode to boot into.
+# After intial start-up, ask what mode to boot into.
+print("Please choose an option:")
+print("1. Admin mode")
+print("2. ATM mode")
+selection = input("Selection: ")
+
+if (selection == "1"): # Boot into admin mode.
+    clear()
+
+    # Ask for an option selection.
     print("Please choose an option:")
-    print("1. Admin mode")
-    print("2. ATM mode")
+    print("1. View public address")
+    print("2. View ATM balance")
+    print("3. View BCH exchange rate")
+    print("4. Generate private key")
     selection = input("Selection: ")
 
-    if (selection == "1"): # Boot into admin mode.
-        clear()
-
-        # Ask for an option selection.
-        print("Please choose an option:")
-        print("1. View public address")
-        print("2. View ATM balance")
-        selection = input("Selection: ")
-
-        if (selection == "1"):
-            print(k.address)
-        elif (selection == "2"):
-            print(k.balance)
-        else:
-            print("Error: Unknown option")
-        
-        
-    elif (selection == "2"): # Boot into normal ATM mode.
-        pass
-    else: # An unknown mode was selected.
-        print("Error: Unknown mode.")
-        print("Exiting")
+    if (selection == "1"):
+        print(k.address)
+        addressqr = pyqrcode.create(k.address)
+        addressqr.svg('publicaddress.svg', scale=2)
+        print(addressqr.terminal(quiet_zone=1))
+    elif (selection == "2"):
+        print(k.balance)
+    elif (selection == "3"):
+        print(satoshi_to_currency(100000000, 'usd'))
+    elif (selection == "4"):
+        k = Key()
+        print("Generated private key: " + k.to_hex())
+    else:
+        print("Error: Unknown option")
+    
+    
+elif (selection == "2"): # Boot into normal ATM mode.
+    pass
+else: # An unknown mode was selected.
+    print("Error: Unknown mode.")
+    print("Exiting")
